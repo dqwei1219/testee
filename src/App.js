8a9d2e7b5c8fb3b0356824bacda9fee1a518c833
Dylan Wei <dqwei@ucsd.edu>
2023-01-10 14:19:26 -0800

    first commit to commit all the files to repository

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+
+// 362b4302
+
+const API_URL = "http://www.omdbapi.com?apikey=362b4302";
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies('Spiderman');
+  }, []);
+
+  return <h1>App</h1>;
+};
+
+export default App;
